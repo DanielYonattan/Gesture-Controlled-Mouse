2179fff42814a89fce40d552fd92f46455d1cfc5
Daniel <danielyonattan@gmail.com>
2024-03-09 01:08:07 -0500

    CHANGE MODEL

--- handtracker.py
+++ handtracker.py
@@ -8,21 +8,21 @@ import cv2 as cv
 def main(): 
     model_path = './../models/hand_landmarker.task'
 
-    HandLandmarker = mp.tasks.vision.HandLandmarker
-    HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
-    HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+    GestureRecognizer = mp.tasks.vision.GestureRecognizer
+    GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+    GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 
     # Create a hand landmarker instance with the live stream mode:
-    def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
         print('hand landmarker result: {}'.format(result.hand_landmarks[0][0] if len(result.hand_landmarks) > 0 else result.hand_landmarks))
  
-    options = HandLandmarkerOptions(
-        base_options=mp.tasks.BaseOptions(model_asset_path='../model/hand_landmarker.task'),
+    options = GestureRecognizerOptions(
+        base_options=mp.tasks.BaseOptions(model_asset_path='../model/gesture_recognizer.task'),
         running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,
         num_hands=1,
         result_callback=print_result)
 
-    with HandLandmarker.create_from_options(options) as landmarker:
+    with GestureRecognizer.create_from_options(options) as landmarker:
     # Use OpenCV’s VideoCapture to start capturing from the webcam.
         vid = cv.VideoCapture(0)
 
@@ -37,7 +37,7 @@ def main():
                 # Convert the frame received from OpenCV to a MediaPipe’s Image object.
                 mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-                landmarker.detect_async(mp_image, mp.Timestamp.from_seconds(time.time()).value)
+                landmarker.recognize_async(mp_image, mp.Timestamp.from_seconds(time.time()).value)
             # the 'q' button is set as the quitting button
             if cv.waitKey(1) & 0xFF == ord('q'): 
                 break
